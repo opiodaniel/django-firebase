3cbb500a74fe36cbd6de7f7ee17ea8ea68dbe83e
Opio Daniel <danielopio540@gmail.com>
2025-09-15 16:16:27 +0300

    update on the cashier page

--- django-firebase/web/djangofirebase/settings.py
+++ django-firebase/web/djangofirebase/settings.py
@@ -82,24 +82,24 @@ TEMPLATES = [
 
 ASGI_APPLICATION = 'djangofirebase.asgi.application'
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            # The 'hosts' key takes a list of URLs
-            "hosts": [os.environ.get("REDIS_URL")],
-        },
-    },
-}
-
 # CHANNEL_LAYERS = {
-#     'default': {
-#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-#         'CONFIG': {
-#             "hosts": [('127.0.0.1', 6379)],
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             # The 'hosts' key takes a list of URLs
+#             "hosts": [os.environ.get("REDIS_URL")],
 #         },
 #     },
 # }
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 # Database
