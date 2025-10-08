56e559ae6750e61e630b14e5a71c8c4cf9bf7df2
Opio Daniel <danielopio540@gmail.com>
2025-10-08 10:02:44 +0300

    old version

--- django-firebase/web/djangofirebase/urls.py
+++ django-firebase/web/djangofirebase/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('flashtech.urls')),
-    path('posts/', include('blog.urls')),
+    # path('', include('blog.urls')),
 ]
 
